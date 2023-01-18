1d931721b2c10968a901c9c3498e06c53f4e9cc6
Lancine Sylla <mdslasso@gmail.com>
2023-01-18 13:58:24 +0100

    modification  de la modal

--- FrontEnd/test.js
+++ FrontEnd/test.js
@@ -1,8 +1,91 @@
 
+
+// Creation des elements du dom
+const body = selectElement("body")
+const galerie = selectElement(".gallery")
+const main = selectElement("main")
+const introduction = selectElement("#introduction")
+const portfolio = selectElement("#portfolio")
+const divBnt = createElement("div")
+const btnTous = createElement("button")
+
+
+//Creation des elements du Formulaire login
+const login = selectElement("#login")
+const sectionLogin = createElement("section")
+const loginText = createElement("h2")
+const labelEmail = createElement("label")
+const labelPassword = createElement("label")
+const loginForm = createElement("form")
+const inputEmail = createElement("input")
+const inputPassword = createElement("input")
+const submitLogin = createElement("input")
+const motDePasseOublier = createElement("a")
+
+setAttributes(labelEmail, { "for": "email" });
+setAttributes(labelPassword, { "for": "password", });
+setAttributes(inputEmail, { "type": "email", "name": "email", "id": "email", "required": "" });
+setAttributes(inputPassword, { "type": "password", "name": "password", "id": "password", "required": "" });
+setAttributes(submitLogin, { "type": "submit", "value": "Se connecter" });
+setAttributes(motDePasseOublier, { "href": "#" });
+
+//Creation des elements admin 
+const sectionAdmin = createElement("div")
+const publierChargement = createElement("button")
+const modeEdition = createElement("button")
+const icoEdit = createElement("i")
+
+icoEdit.setAttribute("class", "fa-solid fa-pen-to-square")
+sectionAdmin.setAttribute("class", "section-admin")
+modeEdition.setAttribute("class", "edition")
+publierChargement.setAttribute("class", "publier-chargement")
+
+
+// Creation des elements de la modale
+const modal = createElement("div")
+const close = createElement("span")
+const modalContent = createElement("div")
+const modalElement = createElement("div")
+const textGalerie = createElement("h3")
+const modalPhotos = createElement("div")
+const hr = createElement("hr")
+const btnAjouter = createElement("button")
+const btnSupprimer = createElement("button")
+
+
+close.innerHTML = "&times;"
+textGalerie.innerText = "Galerie photo"
+btnAjouter.innerText = "Ajouter une photo"
+btnSupprimer.innerText = "Supprimer la galerie"
+
+btnAjouter.setAttribute("class", "button-active")
+modal.setAttribute("class", "modal")
+close.setAttribute("class", "close")
+modalPhotos.setAttribute("id", "modal-photos")
+modalContent.setAttribute("id", "modal-content")
+modalElement.setAttribute("id", "modal-element")
+btnSupprimer.setAttribute("id", "btn-supprimer")
+
+
+modalElement.append(textGalerie, modalPhotos, hr, btnAjouter, btnSupprimer)
+modalContent.append(close, modalElement)
+modal.appendChild(modalContent)
+
+
+// Creations des button pour la gestion des filtres par categorie
+divBnt.setAttribute("class", "btn-categorie")
+btnTous.innerText = "Tous"
+divBnt.append(btnTous)
+portfolio.append(modal, divBnt, galerie)
+
+
+
+
 //function pour creer les elements du dom 
 function createElement(element) {
     return document.createElement(element)
 }
+
 
 //function pour selectionner les elements  du dom
 function selectElement(element) {
@@ -17,87 +100,10 @@ function setAttributes(element, attrs) {
 }
 
 
-// Creation des elements du dom
-const body = selectElement("body")
-const galerie = selectElement(".gallery")
-const main = selectElement("main")
-const introduction = selectElement("#introduction")
-const portfolio = selectElement("#portfolio")
-const loginBtn = selectElement("#login")
-
-
-// Creation des elements du formulaire login
-const loginForm = createElement("form")
-const sectionLogin = createElement("section")
-const loginText = createElement("h2")
-const labelEmail = createElement("label")
-const labelPassword = createElement("label")
-const inputEmail = createElement("input")
-const inputPassword = createElement("input")
-const submitLogin = createElement("input")
-const motDePasseOublier = createElement("a")
-
-setAttributes(labelEmail, { "for": "email" });
-setAttributes(labelPassword, { "for": "password", });
-setAttributes(inputEmail, { "type": "email", "name": "email", "id": "email", "required": "" });
-setAttributes(inputPassword, { "type": "password", "name": "password", "id": "password", "required": "" });
-setAttributes(submitLogin, { "type": "submit", "value": "Se connecter" });
-setAttributes(motDePasseOublier, { "href": "#" });
-
-
-//Creation des elements admin 
-const sectionAdmin = createElement("div")
-const publierChargement = createElement("button")
-const modeEdition = createElement("button")
-const icoEdit = createElement("i")
-
-//Modal des elements de la modal
-const modal = createElement("div")
-const modalEntete = createElement("div")
-const close = createElement("span")
-const retour = createElement("span")
-const modalContent = createElement("div")
-const modalElement = createElement("div")
-const titreGalerie = createElement("h3")
-const modalSectionPhoto = createElement("div")
-const hr = createElement("hr")
-const btnAjouter = createElement("button")
-const btnValider = createElement("button")
-const btnSupprimer = createElement("span")
-
-
-close.innerHTML = "&times;"
-retour.innerHTML = "&leftarrow;"
-btnAjouter.innerText = "Ajouter une photo"
-btnValider.innerText = "Valider"
-btnSupprimer.innerText = "Supprimer la galerie"
-
-modal.setAttribute("class", "modal")
-close.setAttribute("class", "close")
-retour.setAttribute("class", "retour")
-modalContent.setAttribute("id", "modal-content")
-modalElement.setAttribute("id", "modal-element")
-modalEntete.setAttribute("id", "modal-entete")
-modalSectionPhoto.setAttribute("id", "modal-section-photo")
-modal.setAttribute("hr", "#")
-
-
-
-
-
-// Creations des button pour la gestion des filtres par categorie
-const divBnt = createElement("div")
-const btnTous = createElement("button")
-divBnt.setAttribute("class", "btn-categorie")
-btnTous.innerText = "Tous"
-divBnt.append(btnTous)
-portfolio.append(modal, divBnt, galerie)
-
-
-
 
 // Function pour afficher les elements de facon dynamique
 function afficherElements(works) {
+
     for (let work of works) {
         const figure = createElement("figure")
         const img = createElement("img")
@@ -159,7 +165,9 @@ function galerieAfficherPhotos() {
                         divBnt.append(btnCategorie)
 
                         btnCategorie.addEventListener("click", function (event) {
+
                             galerie.innerHTML = "";
+                            event.currentTarget.setAttribute("class", "button-active")
 
                             // Filtrage des works par categories
                             const worksByCategorie = works.filter((works) => {
@@ -168,12 +176,15 @@ function galerieAfficherPhotos() {
 
                             });
 
+
                             afficherElements(worksByCategorie)
+
 
                         });
                     }
 
                 })
+
 
 
             //Affichage de tous les works avec le button "Tous"
@@ -193,30 +204,31 @@ galerieAfficherPhotos()
 
 
 
-// formulaire de connexion
+// Section form Login
 
-loginBtn.addEventListener("click", function () {
-    main.innerHTML = "";
-    loginForm.innerHTML = "";
+login.addEventListener("click", function () {
+    login.setAttribute("class", "active-login")
+    main.innerHTML = ""
+    sectionAdmin.remove()
+
 
     labelEmail.innerText = "Email"
     labelPassword.innerText = "Password"
     motDePasseOublier.innerText = "Mot de passe oublié"
     loginText.innerText = "Login"
 
-    sectionLogin.setAttribute("id", "section-form")
+    sectionLogin.setAttribute("id", "contact")
     loginForm.append(labelEmail, inputEmail, labelPassword, inputPassword, submitLogin)
     sectionLogin.append(loginText, loginForm, motDePasseOublier)
     main.appendChild(sectionLogin)
-    sectionAdmin.remove()
-
-
-})
 
 
 
+});
 
-// connexion
+
+
+// Gestion de la connexion via le formulaire login
 
 loginForm.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -246,29 +258,22 @@ loginForm.addEventListener("submit", function (event) {
             loginText.innerText = ""
 
             if (response.status == 200) {
+
+                //window.location.replace("/FrontEnd");
                 main.innerText = ""
-                modeEdition.innerText = ""
-
-                icoEdit.setAttribute("class", "fa-solid fa-pen-to-square")
-                sectionAdmin.setAttribute("class", "section-admin")
-                modeEdition.setAttribute("class", "edition")
-                publierChargement.setAttribute("class", "publier-chargement")
-
                 modeEdition.innerText = ""
                 publierChargement.innerText = "publier les changements"
 
-                modeEdition.append(icoEdit, "Mode édition")
+                modeEdition.append(icoEdit, " Mode édition ")
                 sectionAdmin.append(modeEdition, publierChargement)
 
                 body.prepend(sectionAdmin)
                 main.append(introduction, portfolio)
 
-
             } else {
 
                 loginText.innerText = "Email ou Mot de passe incorrect"
                 sectionAdmin.remove()
-
 
             }
 
@@ -277,20 +282,11 @@ loginForm.addEventListener("submit", function (event) {
 });
 
 
+// Creation de la modale
+
 modeEdition.addEventListener("click", function (event) {
-    event.preventDefault();
     modal.style.display = "block"
-    modalSectionPhoto.innerHTML = ""
-    modalElement.innerHTML = ""
-    titreGalerie.innerText = "Galerie photo"
-
-
-    modal.appendChild(modalContent)
-
-    modalEntete.appendChild(close)
-    modalContent.append(modalEntete, modalElement)
-    modalElement.append(titreGalerie, modalSectionPhoto, hr, btnAjouter, btnSupprimer)
-
+    modalPhotos.innerHTML = ""
 
     fetch(urlApiWorks)
 
@@ -299,36 +295,26 @@ modeEdition.addEventListener("click", function (event) {
 
                 return response.json();
             }
-        }).then(function (works) {
+        })
+        .then(function (works) {
 
             for (let work of works) {
-
                 const article = createElement("article")
-                const cadreImage = createElement("div")
                 const editWork = createElement("span")
-                const deleteWork = createElement("i")
-                const img = new Image()
-
                 editWork.innerText = "édit"
+                const deleteWork = createElement("i")
+                deleteWork.setAttribute("class", "fa-solid fa-trash-can")
+                const img = new Image()
                 img.src = work.imageUrl
                 img.crossOrigin = "Anonymous";
+                article.append(deleteWork, img, editWork)
+                modalPhotos.append(article)
 
-                deleteWork.setAttribute("class", "fa-solid fa-trash-can")
-                cadreImage.setAttribute("id", "cadre-image")
-
-                cadreImage.append(img, deleteWork)
-                article.append(cadreImage, editWork)
-
-                modalSectionPhoto.append(article)
-
-
-                // Suppression  work 
+                // Supprimer work 
                 deleteWork.addEventListener("click", function (event) {
                     event.preventDefault();
-                    modalElement.innerHTML = ""
 
                     let id = work.id
-
 
                     let options = {
                         method: "DELETE",
@@ -343,60 +329,82 @@ modeEdition.addEventListener("click", function (event) {
                         .then((response) => response.json())
                         .then((json) => console.log(json))
 
-                    console.log(' Delete ID : ', id)
-                    modalElement.append(titreGalerie, modalSectionPhoto, hr, btnAjouter, btnSupprimer)
-
 
                 })
 
+
             }
+
+
 
         })
 
-});
+})
 
 
 
 
-// Ajout d'un nouveau work
+
+
 btnAjouter.addEventListener("click", function (event) {
     event.preventDefault();
-    titreGalerie.innerText = "Ajout photo"
-    modalEntete.innerText = ""
-    modalEntete.append(retour, close)
+    modal.style.display = "block"
+    btnSupprimer.style.display = "none"
+    hr.style.display = "none"
+    btnAjouter.style.display = "none"
+    textGalerie.innerText = "Ajouter photo"
+    modalPhotos.style.display = "none"
+
+    // Creation les elements du formulaire d'ajout de photos
+    const sectionAjout = createElement("section")
+    const ajoutForm = createElement("form")
+    const cadreForm = createElement("div")
+    const image = createElement("input")
+    const title = createElement("input")
+    const category = createElement("input")
+    const labelTitle = createElement("label")
+    const labelCategory = createElement("label")
+    const selectCategory = createElement("select")
+    const optionCategory = createElement("option")
+    const submitAjout = createElement("input")
+
+    labelTitle.innerText = "Titre"
+    labelCategory.innerText = "Catégorie"
 
 
-    modalElement.innerHTML = ""
-    modalElement.append(titreGalerie)
+    sectionAjout.setAttribute("id", "contact")
+    cadreForm.setAttribute("id", "cadre-form")
+    setAttributes(labelTitle, { "for": "Titre" });
+    setAttributes(labelCategory, { "for": "Catégorie", });
+    setAttributes(image, { "type": "file", "name": "image", "id": "image", "accept": "image/png, image/jpg", "required": "" });
+    setAttributes(title, { "type": "text", "name": "title", "id": "title", "required": "" });
+    setAttributes(category, { "type": "text", "name": "category", "id": "category", "required": "" });
+    setAttributes(submitAjout, { "type": "submit", "value": "Valider" });
 
-    modalElement.append(titreGalerie, hr, btnValider)
+    ajoutForm.append(image, labelTitle, title, labelCategory, category, submitAjout)
+    sectionAjout.appendChild(ajoutForm)
+    cadreForm.append(sectionAjout)
+
+    modalElement.appendChild(cadreForm)
+
+
 
 
 })
+
 
 
 
 
 // Modal
+
 close.addEventListener("click", function (event) {
-    event.preventDefault();
     modal.style.display = "none"
-    modalEntete.innerText = ""
-    modalEntete.append(close)
-
-
-})
-
-
-retour.addEventListener("click", function (event) {
-    event.preventDefault();
-    titreGalerie.innerText = "Galerie photo"
-    modal.style.display = "block"
-    modalElement.innerText = ""
-    modalEntete.innerText = ""
-    modalEntete.append(close)
-    modalElement.append(titreGalerie, modalSectionPhoto, hr, btnAjouter, btnSupprimer)
-
+    cadreForm.style.display = "none"
+    btnSupprimer.style.display = "block"
+    textGalerie.innerText = "Galerie photo"
+    btnAjouter.style.display = "block"
+    hr.style.display = "block"
 
 
 })
@@ -405,19 +413,18 @@ retour.addEventListener("click", function (event) {
 window.addEventListener("click", function (event) {
 
     if (event.target == modal) {
+        cadreForm.style.display = "none"
         modal.style.display = "none"
-        modalEntete.innerText = ""
-        modalEntete.append(close)
+        btnSupprimer.style.display = "block"
+        btnAjouter.style.display = "block"
+        textGalerie.innerText = "Galerie photo"
+        hr.style.display = "block"
+
 
 
     }
 
 })
-
-
-
-
-
 
 
 
